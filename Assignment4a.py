b2c1b5215cf649ce349d9b52fe25e62342bac613
lovelifecat <60024066+lovelifecat@users.noreply.github.com>
2022-10-07 19:25:26 -0400

    Update Assignment4a.py

--- Assignment4a.py
+++ Assignment4a.py
@@ -1,15 +1,17 @@
 import requests
-import json
 
 def GetRepoNameAndCommit(username):
     url = 'https://api.github.com/users/%s/repos' % username
     r = requests.get(url)
-    return r.status_code
     for i in range(len(r.json())):
         Name = r.json()[i]['name']
         UrlOfCommit = 'https://api.github.com/repos/%s/%s/commits' % (username, Name)
         c = requests.get(UrlOfCommit)
         Number = len(c.json())
         print(Name + " Number of commit: " + str(Number))
+
+if __name__ == '__main__':
+    username = input("please input your username: ")
+    GetRepoNameAndCommit(username)
 
     
