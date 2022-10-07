05b5522b8a7b76d555b711a21c4ab3fcd1b73720
lovelifecat <60024066+lovelifecat@users.noreply.github.com>
2022-10-07 15:25:09 -0400

    Add files via upload

---
+++ Assignment4a.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+username = input("please input username of github:")
+
+url = 'https://api.github.com/users/%s/repos' % username
+
+r = requests.get(url)
+for i in range(len(r.json())):
+    Name = r.json()[i]['name']
+    UrlOfCommit = 'https://api.github.com/repos/%s/%s/commits' % (username, Name)
+    c = requests.get(UrlOfCommit)
+    Number = len(c.json())
+    print(Name + " Number of commit: " + str(Number))
